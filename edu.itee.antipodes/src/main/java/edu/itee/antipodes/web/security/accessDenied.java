deccc4941e98cbfeca2806c45c7a6de7cda1194e
antulik <antulik@6ddbecba-c468-985f-7958-781444bdec58>
2010-04-26 07:12:27 +0000

    packages structure change

--- edu.itee.antipodes/src/main/java/edu/itee/antipodes/web/security/accessDenied.java
+++ edu.itee.antipodes/src/main/java/edu/itee/antipodes/web/security/accessDenied.java
@@ -1,4 +1,4 @@
-package edu.itee.antipodes.web.account;
+package edu.itee.antipodes.web.security;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
