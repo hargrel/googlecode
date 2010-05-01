211ffe32835963a724e262d7056b3aec9380a7f0
Learner0903 <Learner0903@6ddbecba-c468-985f-7958-781444bdec58>
2010-05-01 05:32:31 +0000

    Query for the Use Case Monitoring search criteria utilisation: SearchForReportingDaoHibernate.java, SearchForReportingDaoHibernateTest.java

---
+++ edu.itee.antipodes/src/main/java/edu/itee/antipodes/repository/SearchForReportingDaoHibernate.java
@@ -0,0 +1,76 @@
+package edu.itee.antipodes.repository;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.springframework.dao.DataAccessResourceFailureException;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+import edu.itee.antipodes.domain.UniversalBean;
+import edu.itee.antipodes.domain.db.ListedTour;
+
+public class SearchForReportingDaoHibernate extends HibernateDaoSupport implements Iterable{
+	Object[] tuple=null;
+
+
+/*
+ * @param minNum the specified minimum number of listed tour associated with criteria activity
+ * @request minNum != null
+ * @return returns a list of tuples(i.e. Object[]), 
+ * in which Object[0]=activityID, Object[1]=activityName, Object[2]= number of
+ * listed tours associated with the criteria
+ * @author 41931527 
+ */
+	public List<Object[]> getNumToursAsscWithActivityByMinNum(int minNum){
+		List<Object[]> list = new ArrayList<Object[]>();
+				
+		String select="select a.activityID, a.activityName, COUNT(l.listID) ";
+		String from="from ListedTour as l join l.tour as t join t.activities as a ";
+		if ((Integer)minNum != null){
+			String sql= select+ from+"group by a.activityID, a.activityName "+"having COUNT(l.listID) >= :minNum";
+			Query query= getSession().createQuery(sql);
+			query.setParameter("minNum", Long.valueOf(minNum));
+			int i=0;
+			 for (Iterator it= query.iterate(); it.hasNext(); i++ ) {
+		            list.add((Object[]) it.next());
+			 }
+		}
+
+		return list;
+	}
+	/*
+	 * @param minNum the specified minimum number of listed tour associated with criteria loction
+	 * @request minNum != null
+	 * @return returns a list of tuples(i.e. Object[]), 
+	 * in which Object[0]=locationID, Object[1]=locationName, Object[2]= number of
+	 * listed tours associated with the criteria
+	 * @author 41931527 
+	 */
+	public List<Object[]> getNumToursAsscWithLocationByMinNum(int minNum){
+		List<Object[]> list = new ArrayList<Object[]>();				
+		String select="select c.locationID, c.locationName, COUNT(l.listID) ";
+		String from="from ListedTour as l join l.tour as t join t.locations as c ";
+		if ((Integer)minNum != null){
+			String sql= select+ from+ "group by c.locationID, c.locationName "+"having COUNT(l.listID) >=:minNum";
+			Query query= getSession().createQuery(sql);
+			query.setParameter("minNum", Long.valueOf(minNum));
+			int i=0;
+			 for (Iterator it= query.iterate(); it.hasNext(); i++ ) {
+		            list.add((Object[]) it.next());
+			 }
+		}
+
+		return list;
+	}
+
+	@Override
+	public Iterator iterator() {
+		return this.iterator();
+	}
+}
---
+++ edu.itee.antipodes/src/test/java/edu/itee/antipodes/test/repository/SearchForReportingDaoHibernateTest.java
@@ -0,0 +1,59 @@
+package edu.itee.antipodes.test.repository;
+
+import static org.junit.Assert.*;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.hibernate.HibernateException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessResourceFailureException;
+
+import edu.itee.antipodes.domain.db.*;
+import edu.itee.antipodes.repository.SearchForReportingDaoHibernate;
+
+public class SearchForReportingDaoHibernateTest extends TestCase {
+	private ApplicationContext ctx = null;
+	private SearchForReportingDaoHibernate dao = null;
+
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+		String[] paths = { "edu/itee/antipodes/repository/hibernate-cfg.xml" };
+		ctx = new ClassPathXmlApplicationContext(paths);
+		dao = (SearchForReportingDaoHibernate) ctx.getBean("reportSearchDao");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		super.tearDown();
+		dao = null;
+	}
+
+	@Test
+	public void testSearch() throws DataAccessResourceFailureException,
+			HibernateException, IllegalStateException, ParseException {
+
+		
+		//test getNumToursAsscWithLocationByMinNum(int minNum)
+//		assertNotNull(dao.getNumToursAsscWithActivityByMinNum(1));
+//		assertEquals(4,dao.getNumToursAsscWithActivityByMinNum(1).size());
+		assertEquals("3",dao.getNumToursAsscWithActivityByMinNum(2).get(0)[2].toString());
+
+		
+		//test getNumToursAsscWithLocationByMinNum(int minNum)
+//		assertEquals(4,dao.getNumToursAsscWithLocationByMinNum(1).size());
+//		assertEquals(2,dao.getNumToursAsscWithLocationByMinNum(2).size());
+		assertEquals("3",dao.getNumToursAsscWithLocationByMinNum(2).get(0)[2].toString());
+		
+	}
+
+}
