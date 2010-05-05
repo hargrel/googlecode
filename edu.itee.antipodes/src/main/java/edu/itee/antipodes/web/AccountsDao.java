1034286624f9e679cb30e34e2e9d68e11eabd4b4
seannysr <seannysr@6ddbecba-c468-985f-7958-781444bdec58>
2010-05-05 05:07:10 +0000

    removed old accounts classes

--- edu.itee.antipodes/src/main/java/edu/itee/antipodes/web/AccountsDao.java
+++
@@ -1,9 +0,0 @@
-package edu.itee.antipodes.web;
-
-
-public interface AccountsDao {
-    public Account readAccount(Long id);
-    public void createOrUpdateAccount(Account account);
-    public Account[] findAccounts();
-
-}
