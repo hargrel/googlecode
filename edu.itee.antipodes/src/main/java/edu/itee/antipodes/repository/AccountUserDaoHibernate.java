bcdf6628ac6e8b8b6fd15a869970f1c488b03b43
tay.booncheen <tay.booncheen@6ddbecba-c468-985f-7958-781444bdec58>
2010-05-18 05:14:28 +0000

    change from query to hibernateTemplate to close session properly.

--- edu.itee.antipodes/src/main/java/edu/itee/antipodes/repository/AccountUserDaoHibernate.java
+++ edu.itee.antipodes/src/main/java/edu/itee/antipodes/repository/AccountUserDaoHibernate.java
@@ -1,89 +1,57 @@
 package edu.itee.antipodes.repository;
 
-import edu.itee.antipodes.domain.db.AccountUser;
-
 import java.util.List;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
+import edu.itee.antipodes.domain.db.AccountUser;
 
-public class AccountUserDaoHibernate /*extends HibernateDaoSupport*/ implements AccountUserDao {
-	private SessionFactory sessionFactory = null;
-	private Session session=null;
-	Query query=null;
-	
+public class AccountUserDaoHibernate extends HibernateDaoSupport implements AccountUserDao {
+
 	public void addAccountUser(AccountUser accUser){
-		session=sessionFactory.openSession();
-		session.getTransaction().begin();
-		session.save(accUser);
-		session.getTransaction().commit();
-
+		getHibernateTemplate().save(accUser);
 	}
 
 	@SuppressWarnings("unchecked")
 	public List<AccountUser> getAccountUserList() {
-		session=sessionFactory.openSession();
-		session.getTransaction().begin();
-		query = session.createQuery("select u from AccountUser as u");
-		List<AccountUser> list = query.list();
-		session.getTransaction().commit();
-
-		return list;
-		
-		
-//		return getHibernateTemplate().find("from AccountUser");
+		return getHibernateTemplate().find("from AccountUser");
 	}
 	
 	public AccountUser getAccountUserByID(int id){
-		session=sessionFactory.openSession();
-		session.getTransaction().begin();
-		AccountUser user= (AccountUser) session.get(AccountUser.class, id);
-		session.getTransaction().commit();
-
-		return user;
-		
-//		return (AccountUser)getHibernateTemplate().get(AccountUser.class, id);
+		return (AccountUser)getHibernateTemplate().get(AccountUser.class, id);
 	}
+	
+	@Autowired
+	SessionFactory mySessionFactory;
+	
+	@SuppressWarnings("unchecked")
 	public AccountUser getAccountUserByUsername(String userName) {
-		session=sessionFactory.openSession();
-		session.getTransaction().begin(); 
-		Query query = session.createQuery("select u from AccountUser as u where u.userName=:userName");
-		query.setParameter("userName", userName);
-		session.getTransaction().commit();
 
-		 return (AccountUser)query.uniqueResult();
-		}
+//		Query query = getSession().createQuery("select u from AccountUser as u where u.userName=:userName");
+//		query.setParameter("userName", userName);
+//		return (AccountUser)query.uniqueResult();
+		
+		if (userName == null || userName.equalsIgnoreCase("roleAnonymous")) return null;
+		List<AccountUser> list = (List<AccountUser>)getHibernateTemplate().findByNamedParam("select u from AccountUser as u where u.userName=:userName", "userName", userName); 
+		if (list.size() == 0) return null;
+		return (AccountUser)list.get(0);
+	}
 	
 	public void dropAccountUser(AccountUser accUser) {
-		session=sessionFactory.openSession();
-		session.getTransaction().begin(); 
-		Object record = session.load(AccountUser.class, accUser.getUserID());
-        session.delete(record);
-        session.getTransaction().commit();
-
+		Object record = getHibernateTemplate().load(AccountUser.class, accUser.getUserID());
+        getHibernateTemplate().delete(record);
     }
 	public void dropAccountUserByID(int accUserID) {
-		session=sessionFactory.openSession();
-		session.getTransaction().begin(); 
-		Object record = session.load(AccountUser.class, accUserID);
-        session.delete(record);
-        session.getTransaction().commit();
-
+		Object record = getHibernateTemplate().load(AccountUser.class, accUserID);
+        getHibernateTemplate().delete(record);
     }
 
 	public void saveAccountUser(AccountUser accUser) {
-		session=sessionFactory.openSession();
-		session.getTransaction().begin(); 
-		session.saveOrUpdate(accUser);
-		session.getTransaction().commit();
-
-	}
-	public SessionFactory getSessionFactory() {
-		return sessionFactory;
-		}
-	public void setSessionFactory(SessionFactory sessionFactory) {
-		this.sessionFactory = sessionFactory;
+		getHibernateTemplate().saveOrUpdate(accUser);
 	}
 }
