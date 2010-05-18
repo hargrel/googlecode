642cfd8afeefa4d544a71cfa6d6bc46cc53e75b1
tay.booncheen <tay.booncheen@6ddbecba-c468-985f-7958-781444bdec58>
2010-05-18 06:51:52 +0000

    change query to hibernateTemplate to close session properly

--- edu.itee.antipodes/src/main/java/edu/itee/antipodes/repository/TourDaoHibernate.java
+++ edu.itee.antipodes/src/main/java/edu/itee/antipodes/repository/TourDaoHibernate.java
@@ -1,5 +1,6 @@
 package edu.itee.antipodes.repository;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.hibernate.HibernateException;
 import org.hibernate.Query;
@@ -9,48 +10,59 @@ import edu.itee.antipodes.domain.db.*;
 
 public class TourDaoHibernate extends HibernateDaoSupport implements TourDao {
 
-        public void addTour(Tour tour) throws HibernateException{
-                getHibernateTemplate().save(tour);
-        }
+	public void addTour(Tour tour) throws HibernateException{
+		getHibernateTemplate().save(tour);
+	}	
+   
+    @SuppressWarnings("unchecked")
+    public List<Tour> getTourList() throws HibernateException{
+		return (List<Tour>)getHibernateTemplate().find("from Tour");
+    }
+   
+    public Tour getTourByID(int id){
+		return (Tour)getHibernateTemplate().get(Tour.class, id);
+    }
+   
+    public void saveTour(Tour tour) throws HibernateException{
+		getHibernateTemplate().saveOrUpdate(tour);
+    }
        
-        @SuppressWarnings("unchecked")
-        public List<Tour> getTourList() throws HibernateException{
-                return (List<Tour>)getHibernateTemplate().find("from Tour");
-        }
-       
-        public Tour getTourByID(int id){
-                return (Tour)getHibernateTemplate().get(Tour.class, id);
-        }
-       
-        public void saveTour(Tour tour) throws HibernateException{
-                getHibernateTemplate().saveOrUpdate(tour);
-        }
-       
-        public void dropTour(Tour tour) throws HibernateException{
-                Object record = getHibernateTemplate().load(Tour.class, tour.getTourID());
+    public void dropTour(Tour tour) throws HibernateException{
+        Object record = getHibernateTemplate().load(Tour.class, tour.getTourID());
         getHibernateTemplate().delete(record);
     }
-        public void dropTourByID(int tourID) throws HibernateException{
-                Object record = getHibernateTemplate().load(Tour.class, tourID);
+
+    public void dropTourByID(int tourID) throws HibernateException{
+        Object record = getHibernateTemplate().load(Tour.class, tourID);
         getHibernateTemplate().delete(record);
     }
         
-        /*
-         * For Operator functionality: get all the tours added (not the listed)by an operator
-         */
-        public  List<Tour> getTourListByOperatorID(int id){
-        	Query query = getSession().createQuery("select t from Tour as t join t.operator as o where o.operatorID=:id ");
-        	List<Tour> list = query.setParameter("id", id).list();
-        	return list;
-        }
-       
-//      public Set<TourDate> getTourDatesByTourID(int tourID){
-//              return ((Tour)getHibernateTemplate().get(Tour.class, tourID)).getTourDates();
-//      }
+	 //For Operator functionality: get all the tours added (not the listed)by an operator
+	@SuppressWarnings("unchecked")
+	public  List<Tour> getTourListByOperatorID(int id){
+		
+		// --------------------- START: original version ------------------------------------------------------------
+		//Query query = getSession().createQuery("select t from Tour as t join t.operator as o where o.operatorID = :id ");
+		//List<Tour> list = query.setParameter("id", id).list();
+		//return list;
+		// --------------------- END: original version ------------------------------------------------------------
+		
+		if (id == 0) return null;
+		List<Tour> list = new ArrayList<Tour>();
+		String sql = "select t from Tour as t join t.operator as o where o.operatorID = :id ";
+		list = (List<Tour>)getHibernateTemplate().findByNamedParam(sql, "id", id);
+		if (list.size() == 0) return null;
+		return list;
+	}
+	
+//	public Set<TourDate> getTourDatesByTourID(int tourID){
+//		return ((Tour)getHibernateTemplate().get(Tour.class, tourID)).getTourDates();
+//	}
 //
-//      public Set<Image> getImagesByTourID(int tourID){
-//              return ((Tour)getHibernateTemplate().get(Tour.class, tourID)).getTourImages();
-//      }
-
+//	public Set<Image> getImagesByTourID(int tourID){
+//		return ((Tour)getHibernateTemplate().get(Tour.class, tourID)).getTourImages();
+//	}
 }
+
+
 
