e0e5953c9ec456b76fc55c5d9ae42ccd8cd9e4b2
tay.booncheen <tay.booncheen@6ddbecba-c468-985f-7958-781444bdec58>
2010-05-19 09:38:41 +0000

    added search by any case and default search all listed tours

--- edu.itee.antipodes/src/main/java/edu/itee/antipodes/repository/CustomerSearchDaoHibernate.java
+++ edu.itee.antipodes/src/main/java/edu/itee/antipodes/repository/CustomerSearchDaoHibernate.java
@@ -4,13 +4,9 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
 import org.hibernate.HibernateException;
-import org.hibernate.Query;
 import org.springframework.dao.DataAccessResourceFailureException;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
-
-import edu.itee.antipodes.domain.db.AccountUser;
 import edu.itee.antipodes.service.UtilityManager;
 
 public class CustomerSearchDaoHibernate extends HibernateDaoSupport implements Iterable<Object>{
@@ -21,57 +17,62 @@ public class CustomerSearchDaoHibernate extends HibernateDaoSupport implements I
 	UtilityManager um = new UtilityManager();
 	
 	@SuppressWarnings("unchecked")
-	public void SearchTourDatesFromName(String tourName){
-//		Query query= getSession().createQuery(select+"from TourDate as d left join d.tour as t where t.tourName=?");
-//		Iterator it= query.setString(0, tourName).iterate();
-//		 while ( it.hasNext() ) {
-//	            tuple= (Object[]) it.next();
-//	            System.out.println(tuple[0].toString());
-//		 }
-//		getSession().close();
-	}
-	
-	@SuppressWarnings("unchecked")
 	public List<Object> findListedTours(String activityNames, String locationNames, String startDate, String finishDate) 
 	throws DataAccessResourceFailureException, HibernateException, IllegalStateException, ParseException{
 		
-		// dummy values to avoid passing null to database
-		String activityNamesTemp = "dummy";
-		String locationNamesTemp = "dummy";
-		String startDateTemp = "22/11/2000";
-		String finishDateTemp = "22/11/3000";
+		// default values for search criteria which will not search anything
+		String activityNamesTemp = "xxx"; 		 
+		String locationNamesTemp = "xxx";		
+		String startDateTemp = "22/11/2000";	 
+		String finishDateTemp = "22/11/9000";
+		String sqlCustomerSearchQuery = "";
+		String superFlag = "super";
+		List<Object> list = new ArrayList<Object>(); 
 		
-		// use input parameters if not null, else use dummy values
-		if (activityNames != null) {activityNamesTemp = activityNames;}
-		if (locationNames != null) {locationNamesTemp = locationNames;}
-		if (startDate != "") {startDateTemp = startDate;}
-		if (finishDate != "") {finishDateTemp = finishDate;}
+		// search all listed tours if NONE of the criteria provided
+		if (activityNames.isEmpty()){
+			if (locationNames.isEmpty()){
+				if (startDate.isEmpty()){
+					if (finishDate.isEmpty()){
+						
+						superFlag = "not super";
+						sqlCustomerSearchQuery = "from ListedTour as l join l.tour as t join t.locations as "+
+							"c join t.activities as a join t.tourDates as d";
+						list = (List<Object>)getHibernateTemplate().find(sqlCustomerSearchQuery);
+					}
+				}
+			}
+		}
+		// search listed tours based on any search criteria provided
+		if (superFlag == "super") { 
+			
+			// update default search criteria
+			if (activityNames != "") {activityNamesTemp = activityNames;}
+			if (locationNames != "") {locationNamesTemp = locationNames;}
+			if (startDate != "") {startDateTemp = startDate;}
+			if (finishDate != "") {finishDateTemp = finishDate;}
+
+			String[] params = new String[4];
+			params[0] = "activityName1"; 
+			params[1] = "locationName1"; 
+			params[2] = "startDate"; 
+			params[3] = "finishDate";
+			
+			Object[] vals = new Object[4];
+			vals[0] = activityNamesTemp.toUpperCase().split(",");
+			vals[1] = locationNamesTemp.toUpperCase().split(",");
+			vals[2] = um.stringToDate(startDateTemp, pattern);
+			vals[3] = um.stringToDate(finishDateTemp, pattern);
+
+			// query listed tours on any locs OR acts, AND with dates
+			sqlCustomerSearchQuery = "from ListedTour as l join l.tour as t " +
+				"join t.locations as c join t.activities as a join t.tourDates as d " +
+				"where (c.locationName IN (:locationName1) or a.activityName IN (:activityName1)) " +
+				"and (d.startDate >= :startDate and d.finishDate <= :finishDate)";
+			
+			list = (List<Object>)getHibernateTemplate().findByNamedParam(sqlCustomerSearchQuery, params, vals);
+		}
 		
-		// query listed tours on any locs OR acts, AND with dates
-		String sqlCustomerSearchQuery = "from ListedTour as l join l.tour as t " +
-			"join t.locations as c join t.activities as a join t.tourDates as d " +
-			"where (c.locationName IN (:locationName1) or a.activityName IN (:activityName1)) " +
-			"and (d.startDate >= :startDate and d.finishDate <= :finishDate)";
-		
-//		// use setParameterList() for multiple values
-//		Query query = getSession().createQuery(sqlCustomerSearchQuery)
-//			.setParameter("startDate", um.stringToDate(startDateTemp, pattern))
-//			.setParameter("finishDate", um.stringToDate(finishDateTemp, pattern))
-//			.setParameterList("activityName1", activityNamesTemp.split(","))
-//			.setParameterList("locationName1", locationNamesTemp.split(","));
-//		return query.list();
-		
-		List<Object> list = new ArrayList<Object>();
-		
-		String[] params = new String[4];
-		params[0] = "activityName1"; params[1] = "locationName1"; params[2] = "startDate"; params[3] = "finishDate";
-		
-		Object[] vals = new Object[4];
-		vals[0] = activityNamesTemp.split(",");
-		vals[1] = locationNamesTemp.split(",");
-		vals[2] = um.stringToDate(startDateTemp, pattern);
-		vals[3] = um.stringToDate(finishDateTemp, pattern);
-		list = (List<Object>)getHibernateTemplate().findByNamedParam(sqlCustomerSearchQuery, params, vals);
 		if (list.size() == 0) return null;
 		return list;	
 	}
