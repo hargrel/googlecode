7cc8c0f0894378f63431974ec074472d33bb3d21
luke.hargreaves <luke.hargreaves@6ddbecba-c468-985f-7958-781444bdec58>
2010-05-20 09:36:18 +0000

    Message "has" been sent

--- edu.itee.antipodes/src/main/java/edu/itee/antipodes/web/customer/contactUsController.java
+++ edu.itee.antipodes/src/main/java/edu/itee/antipodes/web/customer/contactUsController.java
@@ -42,7 +42,7 @@ public final class contactUsController {
 
 		model.addAttribute("headermessage", "Thank you.");
 		model.addAttribute("message",
-				"Your message have been sent. Thank you for your time");
+				"Your message has been sent. Thank you for your time");
 		return "success";
 
 	}
