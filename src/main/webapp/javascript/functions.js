5aea0fd714fcb2c57383d4757f2ea9fe5dbbb375
luke.hargreaves@gmail.com <luke.hargreaves@gmail.com@1a04e4bf-fdbb-0ce7-0ff7-d0ad5b875b2f>
2011-10-21 07:12:48 +0000

    Tweaked speed and acceleration / deceleration functions.

--- src/main/webapp/javascript/functions.js
+++ src/main/webapp/javascript/functions.js
@@ -1,38 +1,66 @@
 /** Custom functions (like static methods) */
 
 function moveObject() {
-	moveDiv("#google", new coordinate(100, 100), new coordinate(500, 500));
+	moveDiv("#google", new coordinate(50, 50), new coordinate(900, 500));
 }
 
 /* Moves an object "#id" from the start "coordinate" to the end "coordinate". */
-/* y = m(x - x1) + y1 */
 function moveDiv(id, start, end) {
-	var slope = Math.floor((end.y - start.y) / (end.x - start.x));
+	var slope = calculateGradient(start, end);
 	var xCount = start.x;
-	var interval = setInterval(mover, 1);
-	var slideDistance = end.x - start.x;
+	var distance = end.x - start.x;
 
+	var interval = setInterval(mover, 1);
 	function mover() {
 		if (xCount < end.x) {
-			var yCount = slope * (xCount - start.x) + start.y;
-			var slidePosition = (xCount + 1) - start.x;
+			var yCount = pointSlopeEquation(slope, xCount, start.x, start.y);
+			var position = (xCount + 1) - start.x;
 			
 			$(id).css("top", (yCount + "px"));
 			$(id).css("left", (xCount + "px"));
 			
-			xCount += slideSpeed(slidePosition, slideDistance);
+			xCount += slideSpeed(position, distance);
 		} else {
 			clearInterval(interval);
 		}		
+	}	
+}
+
+function calculateGradient(start, end) {
+	return (end.y - start.y) / (end.x - start.x);
+}
+
+function pointSlopeEquation(slope, x, x1, y1) {
+	return slope * (x - x1) + y1;
+}
+
+function slideSpeed(position, distance) {
+	var halfway = distance / 2;
+	var minSpeed = 0.2;
+	var topSpeed = 5;
+	var distanceOffset = 10;
+	
+	if (position < halfway) {
+		return accelerationSpeed();
+	} else if (position < distance){
+		return decelerationSpeed();
+	} else {
+		return minSpeed;
 	}
 	
-	function slideSpeed(position, distance) {
-		var halfway = distance / 2;
-		
-		if (position < halfway) {
-			return Math.ceil((position / halfway) * 5);
-		} else {
-			return 5;
+	function accelerationSpeed() {
+		var speed = (position / halfway) * topSpeed;
+		if (speed < minSpeed) {
+			return minSpeed;
 		}
+		return speed;
+	}
+	
+	function decelerationSpeed() {
+		var speed = distanceOffset - (position / distance) * distanceOffset;
+		if (speed < minSpeed) {
+			return minSpeed;
+		}
+		return speed;
 	}
 }
