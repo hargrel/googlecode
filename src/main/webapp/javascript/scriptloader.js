518395b8f639612d9df508ce05e8859ec6553a91
luke.hargreaves@gmail.com <luke.hargreaves@gmail.com@1a04e4bf-fdbb-0ce7-0ff7-d0ad5b875b2f>
2011-11-16 08:29:18 +0000

    Adding random movement from searchbar.

--- src/main/webapp/javascript/scriptloader.js
+++ src/main/webapp/javascript/scriptloader.js
@@ -1,8 +1,10 @@
 /** Writes a set of script tags for loading in HTML in one line */
 
 loadscript('https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js');
+loadscript('https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js');
 loadscript('javascript/screen.js');
 loadscript('javascript/links.js');
+loadscript('javascript/searchbar.js');
 loadscript('javascript/debug.js');
 
 function loadscript(src) {
